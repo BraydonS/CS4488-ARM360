7b3134929ad0ec3f19d49029d8202d02c7cc376d
BraydonS <braydonspaulding1@gmail.com>
2023-01-20 08:25:13 -0700

    Initialize VS project with Qt

---
+++ ARM360/ARM360/ARM360.cpp
@@ -0,0 +1,6 @@
+#include "ARM360.h"
+
+ARM360::ARM360(QWidget *parent)
+    : QWidget(parent)
+{
+}
---
+++ ARM360/ARM360/ARM360.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <QtWidgets/QWidget>
+
+class ARM360 : public QWidget
+{
+    Q_OBJECT
+
+public:
+    ARM360(QWidget *parent = nullptr);
+};
