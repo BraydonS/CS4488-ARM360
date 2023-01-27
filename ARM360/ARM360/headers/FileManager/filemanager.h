84ebb0fa8cd09e4f545aca53efb23fa70618bffd
BraydonS <braydonspaulding1@gmail.com>
2023-01-27 12:25:06 -0700

    Add ProgramState and FileManager headers

---
+++ ARM360/ARM360/headers/FileManager/filemanager.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <string>
+#include <fstream>
+
+class FileManager {
+public:
+
+	static FileManager getInstance();
+	bool checkFile(std::string pathString);
+
+	std::string fileToString(std::string path);
+	std::string readFile(std::string file);
+	std::string getLastErrorMessage();
+
+	bool saveFile(std::string path, std::string content);
+
+private:
+	
+	std::string defaultMessage;
+	std::string lastErrorMessage;
+	std::ofstream writer;
+
+	static FileManager instance;
+
+	FileManager();
+
+	bool setWriteFile(std::string path);
+	void write(std::string toWrite);
+	void closeWrite();
+
+};
