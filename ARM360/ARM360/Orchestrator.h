8c089667e1b80bb4591c59978a6ba24b786c52fa
Autumn Clark <clarautu@isu.edu>
2023-01-27 10:21:11 -0700

    Created header file for Orchestrator

---
+++ ARM360/ARM360/Orchestrator.h
@@ -0,0 +1,43 @@
+// Orchestrator header file
+// Autumn Clark
+// 01-27-2023
+
+#pragma once
+#include <string>
+//#include "ExecutorFacade.h"
+//#include "FleManager.h"
+//#include "Hex4digit.h"
+//#include "HexadecimalConvertor.h"
+//#include "ProgramState"
+#include "TranslatorFacade.h"
+
+class Orchestrator {
+private:
+	//Instance variables
+	static Orchestrator* instancePtr;
+	//ProgramState state;
+	TranslatorFacade translator;
+	//ExecutorFacade executor;
+	//FileManager fileManager;
+	std::string error;
+
+	// Private methods
+	Orchestrator();
+	void resetError();
+
+
+public:
+	// Public methods
+	static Orchestrator getInstance();
+	std::string getError();
+	bool next();
+	void clearProgram();
+	//ProgramState getProgramState();
+	void sendInput(char input[]);
+	char* getOutput();
+	bool translateAndLoad(std::string path);
+	char* convertToHexChars(short number);
+	int convertToInt(char number[]);
+	std::string loadFile(std::string path);
+
+};
