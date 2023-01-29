e681ae7d52098456c5d6d82d013f464d131cd83b
Autumn Clark <clarautu@isu.edu>
2023-01-29 13:47:09 -0700

    Created header file for TranslatorAccess

---
+++ ARM360/ARM360/headers/TranslatorAccess.h
@@ -0,0 +1,12 @@
+// TranslatorAccess header file
+// Autumn Clark
+// 01-29-2023
+
+// Will probably want to consoldate this into another class
+
+#include <string>
+
+class TranslatorAccess {
+public:
+	bool translateAndLoad(std::string path);
+};
