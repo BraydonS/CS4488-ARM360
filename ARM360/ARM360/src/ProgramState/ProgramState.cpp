9232f6ab346bfdabdd6566efa6be8d750deb3910
BraydonS <braydonspaulding1@gmail.com>
2023-02-06 16:35:18 -0700

    Finish ProgramState Implementation

--- ARM360/ARM360/src/ProgramState/ProgramState.cpp
+++ ARM360/ARM360/src/ProgramState/ProgramState.cpp
@@ -1,14 +1,25 @@
 #include "stdafx.h"
 #include "Hex4Digit.h"
 #include "MemoryHistorySpace.h"
+#include <string>
+#include <sstream>
 
-//Author: Braydon Spaulding
-//Date Started: 01/24
+/// <summary>
+/// The ProgramState singleton is a data class operated on by the executor, which stores the machine state in a vector of state arrays
+/// </summary>
+/// <version>
+/// 0.1
+/// </version>
+/// <author>
+/// Traae Bloxham, transpiled and updated by Braydon Spaulding
+/// </author>
 
 class ProgramState {
 
 public:
-	
+	/// <summary>
+	/// Memory size is currently static, could be potentially be passed into constructor in future
+	/// </summary>
 	constexpr static int TOTAL_MEMORY_SPACES = 256;
 
 	std::array<Hex4digit,TOTAL_MEMORY_SPACES> registers;
@@ -16,7 +27,7 @@ public:
 	Hex4digit output;
 	std::vector<MemoryHistorySpace> pcHistory;
 	std::vector<std::array<Hex4digit, TOTAL_MEMORY_SPACES>> memoryStateHistory;
-
+	
 	static ProgramState getInstance() {
 		if (emulationState == nullptr) {
 			emulationState = new ProgramState();
@@ -24,53 +35,109 @@ public:
 		return *emulationState;
 	}
 
-	bool initializeState(std::array<Hex4digit,256> code) {
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="code">An array of Hex4digits composed of user instuctions</param>
+	/// <returns>If the instructions were successfully initialized</returns>
+
+	bool initializeState(std::array<Hex4digit,256> instructions) {
 		bool result = false;
 
-		if ((!code.empty()) && (code.size() > 0)) {
+		if ((!instructions.empty()) && (instructions.size() > 0)) {
 			MemoryHistorySpace pc = MemoryHistorySpace();
 			pc.memoryLocation = 0;
-			pc.value.setValue(code[0].getHexChars());
+			pc.value.setValue(instructions[0].getHexChars());
 			pcHistory.push_back(pc);
 
-			fillOutMemory(code);
-			memoryStateHistory.push_back(code);
+			fillOutMemory(instructions);
+			memoryStateHistory.push_back(instructions);
 
 			result = true;
 		}
 
 		return result;
 	}
-	//TODO Finish this
+
 	void clearProgramState() {
 		for (int i = 0; i < REGISTER_COUNT; i++) {
 			registers[i].setValue(0);
 		}
 
-		for (auto& h : memoryStateHistory) {
-			h.clear();
-		}
 		memoryStateHistory.clear();
 		pcHistory.clear();
 	}
 
-	std::string printableProgramState();
+	/// <summary>
+	/// Creates a summary of the current program state
+	/// </summary>
+	/// <returns>The state summary as a string</returns>
+	std::string printableProgramState() {
+		std::stringstream stateSummary;
+		stateSummary << "Input: " << input.getString() << "\n";
+		stateSummary << "Output: " << output.getString() << "\n";
+		stateSummary << "PC: " << registers.back().getString() << "\n";
+		stateSummary << "Registers: ";
 
-	Hex4digit getMemoryStateValue(int n);
+		for (int i = 0; i < registers.size(); i++) {
+			stateSummary << registers[i].getString() << ", ";
+		}
+		
+		stateSummary << "\n\n";
+
+		stateSummary << "Next: \n";
+		
+		std::array<Hex4digit,256> lastState = memoryStateHistory.back();
+		int numZeroHexDigits = 0;
+		bool seenZeroValue = false;
+
+		// This loop appends information about the next state to the report, ommiting zero blocks for readability
+		for (int i = 0; lastState.size(); i++) {
+			Hex4digit nextStateHex = lastState[i];
+			if (nextStateHex.getValue() == 0) {
+				numZeroHexDigits++;
+				if (!seenZeroValue) {
+					stateSummary <<"\nZero block starts at: " << i << "\n";
+					seenZeroValue = true;
+				}
+				continue;
+			}
+			else if (seenZeroValue) {
+				stateSummary << "\nZero block ends at: " << i-1 << "\n";
+				seenZeroValue = false;
+			}
+
+			stateSummary << nextStateHex.getString() << " ";
+		}
+
+		return stateSummary.str();
+	}
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="n"></param>
+	/// <returns></returns>
+	Hex4digit getMemoryStateValue(int n) {
+		return memoryStateHistory[0][n];
+	}
 
 private:
 	constexpr static int REGISTER_COUNT = 16;
 
 	static ProgramState* emulationState;
 
-	void fillOutMemory(std::array<Hex4digit,256> code) {
-		for (int i = code.size(); i < TOTAL_MEMORY_SPACES; i++) {
-			code[i] = Hex4digit(0);
+	/// <summary>
+	/// Fills out the remaining unused space of a user's instructions with zeroes 
+	/// </summary>
+	/// <param name="instructions">An array of Hex4digit instructions</param>
+	void fillOutMemory(std::array<Hex4digit,256> instructions) {
+		for (int i = instructions.size(); i < TOTAL_MEMORY_SPACES; i++) {
+			instructions[i] = Hex4digit(0);
 		}
 	}
 
 	ProgramState() {
-		emulationState = nullptr;
 
 		for (int i = 0; i < REGISTER_COUNT; i++) {
 			registers[i] = (Hex4digit());
