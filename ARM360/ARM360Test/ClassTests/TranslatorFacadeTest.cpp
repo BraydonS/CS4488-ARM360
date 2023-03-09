77835106bb6c703fe8a7182749e1dc897a2276d7
Autumn Clark <clarautu@isu.edu>
2023-03-09 10:06:12 -0700

    Added tests for class TranslatorFacade

--- ARM360/ARM360Test/ClassTests/TranslatorFacadeTest.cpp
+++ ARM360/ARM360Test/ClassTests/TranslatorFacadeTest.cpp
@@ -12,29 +12,52 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 namespace ARM360 {
 	TEST_CLASS(TranslatorFacadeTest) {
 
+		TranslatorFacade facade;
+
 		// Method that is run before every test
 		TEST_METHOD_INITIALIZE(TranslatorInit) {
-
+			facade = TranslatorFacade();
 		}
 
 		// Method that is run after every test
 		TEST_METHOD_CLEANUP(TranslatorCleanup) {
-
+			delete &facade;
 		}
 
-		TEST_METHOD(methodName) {
+		TEST_METHOD(loadFile) {
 			int expectedValue = -1;
 			int valueToCheck = -1;
 			Assert::AreEqual(expectedValue, valueToCheck);
+		}
+		TEST_METHOD(clearFile) {
+			bool expectedResult = false;
+			facade.clearFile();
+			bool result = facade.isTranslatable();
+
+			Assert::AreEqual(expectedResult, result);
+		}
+		TEST_METHOD(isTranslatable) {
+			bool expectedResult = false;
+			bool result = facade.isTranslatable();
+
+			Assert::AreEqual(expectedResult, result);
+		}
+		TEST_METHOD(translateToMachine) {
+			int expectedValue = -1;
+			int valueToCheck = -1;
+			Assert::AreEqual(expectedValue, valueToCheck);
+		}
+		TEST_METHOD(getLastExceptionMessage) {
+			std::string expectedMessage = "No Error";
+			std::string result = facade.getLastExceptionMessage();
+			
+			Assert::AreEqual(expectedMessage, result);
 		}
 
 		/*
 		* Methods left to test
 		bool loadFile(std::string armFile);
-		void clearFile();
-		bool isTranslatable();
 		std::vector<Hex4digit> translateToMachine();
-		std::string getLastExceptionMessage();
 		*/
 	};
 }
