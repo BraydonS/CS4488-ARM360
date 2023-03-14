103b1a8ed3d0f5f0611cb909e619c7cc6cfd4c18
pratshow <pratshow@isu.edu>
2023-03-14 13:47:27 -0600

    Fixed link error in Tests by adding the directory of the .obj files in the ARM360 project into 'Linker < General < Additional Library Directories'

---
+++ ARM360/ARM360Test/ClassTests/Hex4DigitTest.cpp
@@ -0,0 +1,25 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../../ARM360/headers/stdafx.h"
+#include "../../ARM360/headers/Hex4digit.h"
+#include "../../ARM360/headers/HexadecimalConverter.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ARM360Test {
+	TEST_CLASS(Hex4DigitTest) {
+
+		public:
+
+			// Object initalizaton before every test
+			TEST_METHOD_INITIALIZE(Hex4DigitInit) {
+			}
+
+			// Object deletion after every test
+			TEST_METHOD_CLEANUP(Hex4DigitCleanup) {
+			}
+
+
+
+	};
+}
