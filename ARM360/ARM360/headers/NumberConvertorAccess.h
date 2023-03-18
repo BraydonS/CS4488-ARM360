0ae5a901e79570e6d0374a586bfc9a7586310891
manasush <46699115+manasush@users.noreply.github.com>
2023-03-18 05:42:14 -0600

    Implemented NumberConvertorAccess

--- ARM360/ARM360/headers/NumberConvertorAccess.h
+++ ARM360/ARM360/headers/NumberConvertorAccess.h
@@ -2,9 +2,14 @@
 #pragma once
 #include <string>
 #include "Hex4digit.h"
-
+// Define the NumberConverterAccess class
 class NumberConverterAccess {
 public:
+    // Declare the convertToHexChars method, which takes a short integer as input and returns a char array
+
     char* convertToHexChars(short number);
+
+    // Declare the convertToInt method, which takes a char array as input and returns an integer
+
     int convertToInt(char* number);
 };
--- ARM360/ARM360/src/NumberConvertorAccess.cpp
+++ ARM360/ARM360/src/NumberConvertorAccess.cpp
@@ -1,14 +1,49 @@
+
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
 #include "stdafx.h"
 #include "NumberConvertorAccess.h"
+#include "Hex4digit.h"
 
 
-char* NumberConverterAccess::convertToHexChars(short number) {
-	// Stop compile error
-	char temp[] = {'a','b','c'};
-	return temp;
-}
-int NumberConverterAccess::convertToInt(char* number) {
-	// Stop compile error
-	return -1;
-}
+
+
+
+using namespace std;
+
+class NumberConvertorAccess {
+public:
+
+
+    
+    // Converts a short integer to a char array of its hexadecimal representation
+    // Returns a pointer to the char array
+    virtual char* convertToHexChars(short number) {
+        // Allocate memory for the hex string, which has a maximum length of 4 characters plus the null terminator
+        char* hexString = new char[5];
+        // Use sprintf to convert the number to hexadecimal and store the result in hexString
+        sprintf(hexString, "%04X", number);
+        // Return the pointer to the char array
+        return hexString;
+    }
+
+    // Converts a char array of a hexadecimal representation to an integer
+    // Returns the integer value
+    virtual int convertToInt(char* number) {
+        // Use strtol to convert the hex string to an integer
+        // The base parameter is set to 16 to indicate that the input string is in hexadecimal format
+        int result = strtol(number, NULL, 16);
+        // Return the integer value
+        return result;
+    }
+
+
+
+
+
+
+
+
+};
 
