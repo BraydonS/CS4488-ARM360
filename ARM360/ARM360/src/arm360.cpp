343edce3f8b432d8841175182acb7443490e183d
manasush <46699115+manasush@users.noreply.github.com>
2023-04-27 15:15:20 -0600

    Updated registers highlights and clears out when aborted. hex to dec and dec to hex Convertor textbox highlights when  converted

--- ARM360/ARM360/src/arm360.cpp
+++ ARM360/ARM360/src/arm360.cpp
@@ -32,6 +32,8 @@ ARM360::ARM360(QWidget *parent)
     orc = Orchestrator::getInstance();
     totalStates = 0;
     badBool = false;
+    // Make the window transparent
+    setStyleSheet("background-color: ;");
 }
 
 /// <summary>
@@ -105,6 +107,8 @@ void ARM360::onDecimalToHexClicked() {
 
     ui.txtDTHOutput->clear();
     ui.txtDTHOutput->insertPlainText(hexAsQString);
+    ui.txtDTHOutput->setStyleSheet("background-color: yellow");
+
 }
 
 /// <summary>
@@ -139,6 +143,8 @@ void ARM360::onHexToDecimalClicked() {
 
     ui.txtHTDOutput->clear();
     ui.txtHTDOutput->insertPlainText(decimalAsQString);
+    ui.txtHTDOutput->setStyleSheet("background-color: yellow");
+
 }
 
 // Function to convert a std::array<char> to a QString.
@@ -175,6 +181,15 @@ void ARM360::getRegisters() {
 
     ui.txtPC->insertPlainText(arrayToQString(program->registers[15].getHexChars()));
     ui.txtOut->insertPlainText(QString::fromStdString((program->output.getString())));
+
+
+
+
+
+
+
+
+
 }
 
 void ARM360::highlightRegisters() {
@@ -286,6 +301,37 @@ void ARM360::initializeMemoryTable() {}
 void ARM360::abortProgram() {
     orc->clearProgram();
     clearRegisters();
+
+    //clears the register highlights
+    ui.txtR0->setStyleSheet("");
+    ui.txtR1->setStyleSheet("");
+    ui.txtR2->setStyleSheet("");
+    ui.txtR3->setStyleSheet("");
+    ui.txtR4->setStyleSheet("");
+    ui.txtR5->setStyleSheet("");
+    ui.txtR6->setStyleSheet("");
+    ui.txtR7->setStyleSheet("");
+    ui.txtR8->setStyleSheet("");
+    ui.txtR9->setStyleSheet("");
+    ui.txtRa->setStyleSheet("");
+    ui.txtRb->setStyleSheet("");
+    ui.txtRc->setStyleSheet("");
+    ui.txtRd->setStyleSheet("");
+    ui.txtRe->setStyleSheet("");
+    ui.txtPC->setStyleSheet("");
+    ui.txtOut->setStyleSheet("");
+
+
+
+
+
+
+
+
+    
+
+    
+
 }
 
 // Run the whole program until the end of file is reached or an error occurs.
@@ -294,6 +340,111 @@ void ARM360::runProgram() {
 
     while (true) {
         if (orc->getProgramState()->registers[15].getValue() == -1) {
+            
+            // Highlights the registers, PC and out textbox if updates
+
+            if (ui.txtR0->toPlainText() != "+0000") {
+                //highlight the register 0
+                ui.txtR0->setStyleSheet("background-color: yellow");
+            }
+
+            if (ui.txtR1->toPlainText() != "+0000") {
+                //highlight the registers 1
+                ui.txtR1->setStyleSheet("background-color: yellow");
+            }
+
+            if (ui.txtR2->toPlainText() != "+0000") {
+                //highlight the register 2
+                ui.txtR2->setStyleSheet("background-color: yellow");
+            }
+
+
+            if (ui.txtR3->toPlainText() != "+0000") {
+                //highlight the register 3
+                ui.txtR3->setStyleSheet("background-color: yellow");
+            }
+
+
+            if (ui.txtR4->toPlainText() != "+0000") {
+                //highlight the register 4
+                ui.txtR4->setStyleSheet("background-color: yellow");
+            }
+
+
+            if (ui.txtR5->toPlainText() != "+0000") {
+                //highlight the register 5
+                ui.txtR5->setStyleSheet("background-color: yellow");
+            }
+
+
+            if (ui.txtR6->toPlainText() != "+0000") {
+                //highlight the register 6
+                ui.txtR6->setStyleSheet("background-color: yellow");
+            }
+
+
+            if (ui.txtR7->toPlainText() != "+0000") {
+                //highlight the register 7
+                ui.txtR7->setStyleSheet("background-color: yellow");
+            }
+
+
+            if (ui.txtR8->toPlainText() != "+0000") {
+                //highlight the register 8
+                ui.txtR8->setStyleSheet("background-color: yellow");
+            }
+
+
+            if (ui.txtR9->toPlainText() != "+0000") {
+                //highlight the register 9
+                ui.txtR9->setStyleSheet("background-color: yellow");
+            }
+
+            if (ui.txtRa->toPlainText() != "+0000") {
+                //highlight the register a
+                ui.txtRa->setStyleSheet("background-color: yellow");
+            }
+
+            if (ui.txtRb->toPlainText() != "+0000") {
+                //highlight the register b
+                ui.txtRb->setStyleSheet("background-color: yellow");
+            }
+
+            if (ui.txtRc->toPlainText() != "+0000") {
+                //highlight the register c
+                ui.txtRc->setStyleSheet("background-color: yellow");
+            }
+
+            if (ui.txtRd->toPlainText() != "+0000") {
+                //highlight the register d
+                ui.txtRd->setStyleSheet("background-color: yellow");
+            }
+
+            if (ui.txtRe->toPlainText() != "+0000") {
+                //highlight the register e
+                ui.txtRe->setStyleSheet("background-color: yellow");
+            }
+
+            if (ui.txtPC->toPlainText() != "+0000") {
+                //highlight the register PC
+                ui.txtPC->setStyleSheet("background-color: yellow");
+            }
+
+
+            if (ui.txtOut->toPlainText() != "+0000") {
+                //highlight the register Out
+                ui.txtOut->setStyleSheet("background-color: yellow");
+            }
+            
+            
+            
+
+
+
+
+
+
+
             QMessageBox::information(this, tr("Complete"), tr("The end of the file has been reached."));
             break;
         }
